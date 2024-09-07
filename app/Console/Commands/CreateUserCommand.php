3fdef9839d1dcc4d0ba4626ea00ae2df4c4513f6
Vlad <vladmihailov2002@gmail.com>
2024-09-07 18:28:42 +0600

    cloudflare

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-user-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->ask('Enter the name');
+
+        $email = $this->ask('Enter the email');
+
+        $password = $this->secret('Enter the password');
+
+        if ($this->confirm('Do you wish to create this user?')) {
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password),
+            ]);
+
+            $this->info("User {$user->name} created successfully!");
+        } else {
+            $this->warn('Operation cancelled.');
+        }
+
+        return 0;
+    }
+}
---
+++ sh/install.sh
@@ -0,0 +1,13 @@
+composer i
+
+touch database/database.sqlite
+
+php artisan migrate
+
+npm i
+
+npm run build
+
+php artisan app:create-user-command
+
+php artisan serve
